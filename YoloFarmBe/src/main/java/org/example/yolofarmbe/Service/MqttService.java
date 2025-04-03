abcc05d3ac79a1c700cce4ba7f95a16718a72005
toandemoo <toanjhino1@gmail.com>
2025-04-03 10:52:35 +0700

    test

--- YoloFarmBe/src/main/java/org/example/yolofarmbe/Service/MqttService.java
+++ YoloFarmBe/src/main/java/org/example/yolofarmbe/Service/MqttService.java
@@ -34,16 +34,16 @@ import org.springframework.web.client.RestTemplate;
 public class MqttService {
 
    private final String BROKER_URL = "tcp://io.adafruit.com:1883";
-   private final String USERNAME = "phuctrangeo1709";
-   private final String AIO_KEY = "aio_voqw706gj4OynKPHPnLKY7bz5Lqh";
+   private final String USERNAME = "fungchill";
+   private final String AIO_KEY = "aio_qhOp85Q5RZjfaYcpSMyizOk0gTDp";
 
-   private final String FEED_TEMPERATURE = "temperature";
+   // private final String FEED_TEMPERATURE = "temperature";
    private final String FEED_MOISTURE = "moisture";
-   private final String FEED_HUMIDITY = "humidity";
+   // private final String FEED_HUMIDITY = "humidity";
    private final String FEED_LIGHT = "light";
-   private final String FEED_AMOUNTOFWATER = "amountwater";
-   private final String FEED_ATOGGLE = "a-toggle";
-   private final String FEED_MTOGGLE = "m-toggle";
+   private final String FEED_WATER = "water";
+   private final String FEED_FAN = "fan";
+   // private final String FEED_MTOGGLE = "m-toggle";
 
    private MqttClient client;
 
@@ -71,7 +71,7 @@ public class MqttService {
       }
    }
 
-   @Scheduled(fixedRate = 30 * 1000) // Gọi API mỗi 5 giây
+   @Scheduled(fixedRate = 30 * 1000)
    public void fetchDataFromAdafruit() {
       try {
          HttpHeaders headers = new HttpHeaders();
@@ -79,33 +79,33 @@ public class MqttService {
 
          HttpEntity<String> entity = new HttpEntity<>(headers);
 
-         List<String> feeds = Arrays.asList(FEED_TEMPERATURE, FEED_MOISTURE, FEED_HUMIDITY, FEED_ATOGGLE, FEED_MTOGGLE,
-               FEED_AMOUNTOFWATER);
+         List<String> feeds = Arrays.asList(FEED_MOISTURE, FEED_WATER, FEED_FAN, FEED_LIGHT);
 
          for (String feed : feeds) {
 
-            if (feed.equals(FEED_AMOUNTOFWATER)) {
-               String URL = "https://io.adafruit.com/api/v2/" + USERNAME + "/feeds/" + feed
-                     + "/data?limit=1";
-               ResponseEntity<List<AdafruitResponse>> response = restTemplate.exchange(URL,
-                     HttpMethod.GET, entity,
-                     new ParameterizedTypeReference<List<AdafruitResponse>>() {
-                     });
+            // if (feed.equals(FEED_WATER)) {
+            // String URL = "https://io.adafruit.com/api/v2/" + USERNAME + "/feeds/" + feed
+            // + "/data?limit=1";
+            // ResponseEntity<List<AdafruitResponse>> response = restTemplate.exchange(URL,
+            // HttpMethod.GET, entity,
+            // new ParameterizedTypeReference<List<AdafruitResponse>>() {
+            // });
 
-               Integer farm_id = 1;
-               Farm farm = farmRepository.findById(farm_id)
-                     .orElseThrow(() -> new ResourceNotFoundException("Farm with id " + farm_id +
-                           "not exist"));
+            // Integer farm_id = 1;
+            // Farm farm = farmRepository.findById(farm_id)
+            // .orElseThrow(() -> new ResourceNotFoundException("Farm with id " + farm_id +
+            // "not exist"));
 
-               AmountOfWaterRecord amountofwaterRecord = new AmountOfWaterRecord();
-               amountofwaterRecord.setRecordValue(Double.parseDouble(response.getBody().get(0).getValue()));
-               amountofwaterRecord.setRecordTime(Instant.now());
-               amountofwaterRecord.setFarm(farm);
-               recordService.SaveRecords(feed, amountofwaterRecord);
+            // AmountOfWaterRecord amountofwaterRecord = new AmountOfWaterRecord();
+            // amountofwaterRecord.setRecordValue(Double.parseDouble(response.getBody().get(0).getValue()));
+            // amountofwaterRecord.setRecordTime(Instant.now());
+            // amountofwaterRecord.setFarm(farm);
+            // recordService.SaveRecords(feed, amountofwaterRecord);
 
-               System.out.println("Fetched data from " + feed + ": " +
-                     response.getBody().get(0).getValue());
-            } else {
+            // System.out.println("Fetched data from " + feed + ": " +
+            // response.getBody().get(0).getValue());
+            // } else
+            {
                String URL = "https://io.adafruit.com/api/v2/" + USERNAME + "/feeds/" + feed
                      + "/data/last";
                ResponseEntity<AdafruitResponse> response = restTemplate.exchange(URL,
@@ -117,25 +117,29 @@ public class MqttService {
                      .orElseThrow(() -> new ResourceNotFoundException("Farm with id " + farm_id +
                            "not exist"));
 
-               if (feed.equals(FEED_TEMPERATURE)) {
-                  TemperatureRecord temperatureRecord = new TemperatureRecord();
-                  temperatureRecord.setRecordValue(Double.parseDouble(response.getBody().getValue()));
-                  temperatureRecord.setRecordTime(Instant.now());
-                  temperatureRecord.setFarm(farm);
-                  recordService.SaveRecords(feed, temperatureRecord);
-               } else if (feed.equals(FEED_MOISTURE)) {
+               // if (feed.equals(FEED_TEMPERATURE)) {
+               // TemperatureRecord temperatureRecord = new TemperatureRecord();
+               // temperatureRecord.setRecordValue(Double.parseDouble(response.getBody().getValue()));
+               // temperatureRecord.setRecordTime(Instant.now());
+               // temperatureRecord.setFarm(farm);
+               // recordService.SaveRecords(feed, temperatureRecord);
+               // } else
+               if (feed.equals(FEED_MOISTURE)) {
                   MoistureRecord moistureRecord = new MoistureRecord();
                   moistureRecord.setRecordValue(Double.parseDouble(response.getBody().getValue()));
                   moistureRecord.setRecordTime(Instant.now());
                   moistureRecord.setFarm(farm);
                   recordService.SaveRecords(feed, moistureRecord);
-               } else if (feed.equals(FEED_HUMIDITY)) {
-                  HumidityRecord humidityRecord = new HumidityRecord();
-                  humidityRecord.setRecordValue(Double.parseDouble(response.getBody().getValue()));
-                  humidityRecord.setRecordTime(Instant.now());
-                  humidityRecord.setFarm(farm);
-                  recordService.SaveRecords(feed, humidityRecord);
-               } else if (feed.equals(FEED_LIGHT)) {
+               }
+               // else
+               // if (feed.equals(FEED_HUMIDITY)) {
+               // HumidityRecord humidityRecord = new HumidityRecord();
+               // humidityRecord.setRecordValue(Double.parseDouble(response.getBody().getValue()));
+               // humidityRecord.setRecordTime(Instant.now());
+               // humidityRecord.setFarm(farm);
+               // recordService.SaveRecords(feed, humidityRecord);
+               // } else
+               if (feed.equals(FEED_LIGHT)) {
                   LightRecord lightRecord = new LightRecord();
                   lightRecord.setRecordValue(Double.parseDouble(response.getBody().getValue()));
                   lightRecord.setRecordTime(Instant.now());
@@ -151,17 +155,17 @@ public class MqttService {
 
          // Random random = new Random();
          // int x = random.nextInt(10);
-         // publishMessage(FEED_TEMPERATURE, String.valueOf(random.nextInt(60)));
+         // // publishMessage(FEED_TEMPERATURE, String.valueOf(random.nextInt(60)));
          // publishMessage(FEED_MOISTURE, String.valueOf(random.nextInt(60)));
-         // publishMessage(FEED_HUMIDITY, String.valueOf(random.nextInt(60)));
-         // publishMessage(FEED_AMOUNTOFWATER, String.valueOf(random.nextDouble(1)));
+         // // publishMessage(FEED_HUMIDITY, String.valueOf(random.nextInt(60)));
+         // publishMessage(FEED_WATER, String.valueOf(random.nextDouble(1)));
          // if (x % 2 == 0) {
-         // publishMessage(FEED_ATOGGLE, "AW-0");
-         // publishMessage(FEED_MTOGGLE, "MW-0");
+         // publishMessage(FEED_FAN, "0");
+         // // publishMessage(FEED_MTOGGLE, "MW-0");
          // x++;
          // } else {
-         // publishMessage(FEED_ATOGGLE, "AW-1");
-         // publishMessage(FEED_MTOGGLE, "MW-1");
+         // publishMessage(FEED_FAN, "1");
+         // // publishMessage(FEED_MTOGGLE, "MW-1");
          // x++;
          // }
 
